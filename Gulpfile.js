166eea5e377647c6a002291db6adcffb06af08a2
Shady Sharaf <shady@sharaf.me>
2014-08-09 13:57:58 +0300

    Integrate Gulp, and nodemon

---
+++ Gulpfile.js
@@ -0,0 +1,36 @@
+var gulp = require('gulp')
+	, nodemon = require('gulp-nodemon')
+	, jshint = require('gulp-jshint')
+	, sass = require('gulp-sass')
+	, coffee = require('gulp-coffee')
+	, nodemonConfig = require('./nodemon.json')
+	, paths = {
+		styles: 'src/scss/*.scss',
+		scripts: ['src/js/*.js', 'src/coffee/*.coffee']
+	}
+
+gulp.task('scripts', function() {
+	return gulp.src(paths.scripts)
+			.pipe(coffee())
+			.pipe(gulp.dest('public/js'));
+});
+
+gulp.task('styles', function() {
+	return gulp.src('./src/scss/*.scss')
+		.pipe(sass())
+		.pipe(gulp.dest('./public/css'));
+});
+
+gulp.task('compile', ['styles', 'scripts']);
+
+// Rerun the task when a file changes
+gulp.task('watch', function() {
+  gulp.watch(paths.styles, ['styles']);
+  gulp.watch(paths.scripts, ['scripts']);
+});
+
+gulp.task('nodemon', function () {
+	nodemon(nodemonConfig);
+});
+
+gulp.task('default', ['compile', 'watch', 'nodemon']);
