81784a35592f7d9bba96e3e712d1908bf9a23cc6
André Drougge <andre.drougge@gmail.com>
2014-08-15 12:49:35 +0300

    Slim up user model, remove front-end stuff. refs #24

--- includes/user.js
+++ includes/user.js
@@ -1,43 +1,11 @@
 /**
- * Handle authorization, cookie detection, and misc User stuff!
- */
-var user = function(req, res, next){
-    res.locals.auth = false;
-    if ( req.cookies.get('user_id') ) {
-        req.app.get('db').get('users').findById(req.cookies.get('user_id'))
-            .then(function(doc) {
-                req.user = doc;
-                res.locals.auth = true;
-                next();
-            }, function(){
-                next();
-            });
-    } else {
-        next();
-    }
-};
-
-module.exports = user;
-
-/**
  * Handle user registration
  */
-var Register = function() {
+var user = function() {
     this.save = function(data) {
         console.log('Saving user..');
         console.log(data);
-    };
-
-    this.init = function() {
-        console.log('Initiating registration..');
-        this.attachSubmitHandler();
-    };
-
-    this.attachSubmitHandler = function() {
-        $('input[type=submit]').on('click', function() {
-            console.log('Submitting..');
-        });
-    };
+    }
 };
 
-module.exports.Register = Register;
+module.exports.User = user;
