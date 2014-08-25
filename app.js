da67babfb3e8e62d9e4c8c381853b9af5f8dfe84
Shady Sharaf <shady@sharaf.me>
2014-08-25 21:11:27 +0300

    CS fix

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 #!/usr/bin/env node
-var debug = require('debug')('remember')
-    , config = require('./config.json')
-    , server = require('./server/server')
-    , app = server.listen(server.get('port'), function() {
+var debug = require('debug')('remember'),
+    config = require('./config.json'),
+    server = require('./server/server'),
+    app = server.listen(server.get('port'), function() {
         debug('Express server listening on port ' + app.address().port);
     });
 
