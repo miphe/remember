9d16b88595f1982b05d48445b981cacdc4d69000
Pratomo Ardianto <ardi@x-team.com>
2014-08-28 17:13:45 -0700

    Added js files, including bootstrapjs

--- client/js/app.js
+++ client/js/app.js
@@ -14,11 +14,13 @@ angular.module('myApp', [
     'myApp.filters',
     'myApp.services',
     'myApp.directives',
-    'myApp.controllers'
+    'myApp.controllers',
+    'ui.bootstrap'
 ]).
     config(['$routeProvider', function($routeProvider) {
         $routeProvider
             .when('/view1', {templateUrl: './partials/partial1.html', controller: 'MyCtrl1'})
-            .when('/view2', {templateUrl: './partials/partial2.html', controller: 'MyCtrl2'})
+            // .when('/view2', {templateUrl: './partials/partial2.html', controller: 'MyCtrl2'})
+            .when('/view2', {templateUrl: './partials/partial2.html', controller: 'ButtonsCtrl'})
             .otherwise({redirectTo: '/view1'});
     }]);
