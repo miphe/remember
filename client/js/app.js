24aee97b9a44721699247ddd05dd8e5674618931
Shady Sharaf <shady@sharaf.me>
2014-08-31 23:19:56 +0300

    Add Front-end Authentication module in AngularJS

--- client/js/app.js
+++ client/js/app.js
@@ -3,22 +3,12 @@
 var angular = require('angular'),
     ngRoute = require('angular-route');
 
-require('./controllers');
-require('./directives');
-require('./filters');
-require('./services');
+require( './modules/AuthService' );
+require( './controllers/ApplicationController' );
 
 // Declare app level module which depends on filters, and services
 angular.module('myApp', [
     'ngRoute',
-    'myApp.filters',
-    'myApp.services',
-    'myApp.directives',
+    'Authentication',
     'myApp.controllers'
-]).
-    config(['$routeProvider', function($routeProvider) {
-        $routeProvider
-            .when('/view1', {templateUrl: './partials/partial1.html', controller: 'MyCtrl1'})
-            .when('/view2', {templateUrl: './partials/partial2.html', controller: 'MyCtrl2'})
-            .otherwise({redirectTo: '/view1'});
-    }]);
+]);
