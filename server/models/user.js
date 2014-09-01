bd0e2f8ce4bddc0501a1e0c68b2eaaaa4a65c9b1
Shady Sharaf <shady@sharaf.me>
2014-09-01 18:46:56 +0300

    Encrypt user passwords

--- server/models/user.js
+++ server/models/user.js
@@ -1,4 +1,5 @@
 var crypto = require('crypto'),
+    bcrypt = require('bcrypt-nodejs'),
     db = require('../server').get('db');
 
 /**
@@ -20,7 +21,7 @@ User.register = function (email, password) {
             return db.get('users')
                 .insert({
                     email:    email,
-                    password: password
+                    password: bcrypt.hashSync(password)
                 })
                 .then(User.prepareDoc);
         }, function () {
@@ -38,16 +39,18 @@ User.register = function (email, password) {
 User.login = function (email, password) {
     var promise = db.get('users')
         .findOne({email: email})
+        // Check password
         .then(function (doc) {
-            // Check password
-            if ( doc.password === password ) {
-                return User.prepareDoc(doc); // Return the user object, plus token
+            if ( bcrypt.compareSync(password, doc.password) ) {
+                return doc;
             } else {
-                promise.reject( new Error('Invalid password', 'invalid-password') );
+                promise.reject(new Error('Invalid password', 'invalid-password'));
             }
-        }, function () {
-            promise.reject( new Error('Invalid email, not registered', 'email-not-found') );
-        });
+        }, function(){
+            promise.reject(new Error('Invalid email, not registered', 'email-not-found'));
+        })
+        // Return user
+        .then(User.prepareDoc);
     return promise;
 };
 
@@ -60,7 +63,7 @@ User.available = function (email) {
     var promise = db.get('users').find({email: email})
         .then(function (docs) {
             if ( docs.length ) {
-                promise.reject( new Error('Email taken', 'email-taken') );
+                promise.reject(new Error('Email taken', 'email-taken'));
             } else {
                 promise.resolve();
             }
@@ -89,6 +92,7 @@ User.prepareDoc = function (user) {
     return User.createToken(user.email)
         .then(function (doc) {
             delete user.password;
+            delete user._id;
             return {user: user, token: doc.token};
         });
 };
