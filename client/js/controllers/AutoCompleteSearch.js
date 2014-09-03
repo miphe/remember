a8341cd98285d1aec410f0a750b0acedcf375ae4
Shady Sharaf <shady@sharaf.me>
2014-09-03 10:06:51 +0300

    Fix one last mention of q vs keyword var name, and CS reformat

--- client/js/controllers/AutoCompleteSearch.js
+++ client/js/controllers/AutoCompleteSearch.js
@@ -3,20 +3,20 @@
 
 /* Controllers */
 angular.module('myApp.controllers')
-    .controller('AutoCompleteSearch', ['$scope', '$http', function($scope, $http) {
+    .controller('AutoCompleteSearch', ['$scope', '$http', function ($scope, $http) {
         var search = this;
         search.result = [];
 
-        $scope.change = function() {
+        $scope.change = function () {
             search.keyword = $scope.keyword;
-            if ( ! search.keyword ) {
+            if ( !search.keyword ) {
                 search.results = [];
                 return;
             }
 
-            $http.get('/api/search?q=' + search.keyword)
-                .then(function(data) {
-                    if (typeof data.hits !== 'undefined') {
+            $http.get('/api/search?keyword=' + search.keyword)
+                .then(function (data) {
+                    if ( typeof data.hits !== 'undefined' ) {
                         console.log(data.hits.hits)
                         search.result = data.hits.hits;
                     }
