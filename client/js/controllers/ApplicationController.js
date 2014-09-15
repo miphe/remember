62c220326b6ad77919ea8ebb1ecc88039fc7f919
André Drougge <andre.drougge@gmail.com>
2014-09-15 17:06:28 +0300

    Add templating structure (partializing).
    - Partialize templates, making index.html work as a layout.
    - Add dummy template (placeholders).

--- client/js/controllers/ApplicationController.js
+++ client/js/controllers/ApplicationController.js
@@ -3,6 +3,23 @@
 
 angular.module('myApp.controllers', ['Authentication'])
     .controller('ApplicationController', ['$scope', 'AuthService', function ($scope, AuthService) {
+
+        // Layout & templates
+        $scope.tpls = [
+            { name: 'header', url: 'partials/header.html'}, // 0
+            { name: 'search', url: 'partials/search.html'}, // 1
+            { name: 'view',   url: 'partials/view.html'},   // 2
+            { name: 'write',  url: 'partials/write.html'}   // 3
+        ];
+
+        $scope.tpl = {
+            header: $scope.tpls[0],
+            search: $scope.tpls[1],
+            view:   $scope.tpls[2],
+            write:  $scope.tpls[3]
+        };
+
+        // User
         $scope.currentUser = null;
         $scope.isLogged = AuthService.isAuthenticated;
 
