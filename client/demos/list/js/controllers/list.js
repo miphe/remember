22c324b35d92185bd2577cedb176ee69fddd9256
Bernardo Dias <bernardodiasdacruz@gmail.com>
2014-09-16 19:29:26 -0300

    markup and style improvement, content from controller

--- client/demos/list/js/controllers/list.js
+++ client/demos/list/js/controllers/list.js
@@ -1,1 +1,45 @@
-var listApp = angular.module('listApp', []);
+app.controller('listController', ['$scope', function ($scope) {
+
+    $scope.entries = [
+        {
+            date: "321321",
+            content: "<h1>Lorem ipsum dolor sit amet</h1>" +
+                    "<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit quo quam aspernatur. In et sit voluptates impedit nisi laborum consequuntur ab earum maxime aspernatur, sint at quaerat illo reiciendis quibusdam. Velit quo quam aspernatur. In et sit voluptates impedit nisi laborum consequuntur ab earum maxime aspernatur, sint at quaerat illo reiciendis quibusdam.</p>" +
+                    "<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit quo quam aspernatur. In et sit voluptates impedit nisi laborum consequuntur ab earum maxime aspernatur, sint at quaerat illo reiciendis quibusdam.</p>",
+            author: "hammerizer"
+        },
+        {
+            date: "12121212",
+            content: "<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit quo quam aspernatur. In et sit voluptates impedit nisi laborum consequuntur ab earum maxime aspernatur, sint at quaerat illo reiciendis quibusdam.</p>",
+            author: "hammerizer"
+        },
+        {
+            date: "09090909",
+            content: "<h1>Lorem ipsum dolor sit amet</h1>" +
+                    "<p>Eveniet eligendi enim neque, autem voluptate. Velit quo quam aspernatur. In et sit voluptates impedit nisi laborum consequuntur ab earum maxime aspernatur, sint at quaerat illo reiciendis quibusdam.</p>" +
+                    "<h2>In et sit voluptates</h2>" +
+                    "<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit quo quam aspernatur. In et sit voluptates impedit nisi laborum consequuntur ab earum maxime aspernatur, sint at quaerat illo reiciendis quibusdam. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit quo quam aspernatur. In et sit voluptates impedit nisi laborum consequuntur ab earum maxime aspernatur, sint at quaerat illo reiciendis quibusdam. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit quo quam aspernatur. In et sit voluptates impedit nisi laborum consequuntur ab earum maxime aspernatur, sint at quaerat illo reiciendis quibusdam. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit quo quam aspernatur. In et sit voluptates impedit nisi laborum consequuntur ab earum maxime aspernatur, sint at quaerat illo reiciendis quibusdam.</p>" +
+                    "<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit quo quam aspernatur. In et sit voluptates impedit nisi laborum consequuntur ab earum maxime aspernatur, sint at quaerat illo reiciendis quibusdam. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit quo quam aspernatur. In et sit voluptates impedit nisi laborum consequuntur ab earum maxime aspernatur, sint at quaerat illo reiciendis quibusdam. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit quo quam aspernatur. In et sit voluptates impedit nisi laborum consequuntur ab earum maxime aspernatur, sint at quaerat illo reiciendis quibusdam. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit quo quam aspernatur. In et sit voluptates impedit nisi laborum consequuntur ab earum maxime aspernatur, sint at quaerat illo reiciendis quibusdam.</p>" +
+                    "<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit quo quam aspernatur. In et sit voluptates impedit nisi laborum consequuntur ab earum maxime aspernatur, sint at quaerat illo reiciendis quibusdam.</p>",
+            author: "hammerizer"
+        },
+        {
+            date: "09090909",
+            content: "<h1>Lorem ipsum dolor sit amet</h1>" +
+                    "<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit quo quam aspernatur. In et sit voluptates impedit nisi laborum consequuntur ab earum maxime aspernatur, sint at quaerat illo reiciendis quibusdam.</p>" +
+                    "<pre>" +
+                    "article {" +
+                    "    max-height: 200px;" +
+                    "    overflow: hidden;" +
+                    "    border: 1px solid #ddd;" +
+                    "    border-radius: 4px 4px 0 0;" +
+                    "    padding: 10px;" +
+                    "    margin: 0 0 15px;" +
+                    "}" +
+                    "</pre>" +
+                    "<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit quo quam aspernatur. In et sit voluptates impedit nisi laborum consequuntur ab earum maxime aspernatur, sint at quaerat illo reiciendis quibusdam.</p>" +
+                    "<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit quo quam aspernatur. In et sit voluptates impedit nisi laborum consequuntur ab earum maxime aspernatur, sint at quaerat illo reiciendis quibusdam.</p>",
+            author: "hammerizer"
+        }
+    ];
+}]);
---
+++ client/demos/list/js/controllers/search.js
@@ -0,0 +1,5 @@
+app.controller('searchController', ['$scope', function ($scope) {
+	// todo:
+	// filter results when typing
+	// maybe not on this demo, this should use the power of elastic search
+}]);
