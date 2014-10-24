0a571561326d11e967e7133ad448dc68c49168bb
André Drougge <andre.drougge@gmail.com>
2014-10-24 15:32:19 +0300

    Add test suite to gulp browserify tasks/watchers
    refs #80

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp'),
     tasks = [
         'browserify',
+        'browserify_tests',
         'sass',
         'bower',
         'watch',
@@ -14,4 +15,4 @@ tasks.forEach(function(name) {
 });
 
 gulp.task('build', ['browserify', 'sass', 'lint', 'bower']);
-gulp.task('default', ['browserify', 'sass', 'watch', 'nodemon']);
+gulp.task('default', ['browserify', 'browserify_tests', 'sass', 'watch', 'nodemon']);
--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -3,5 +3,5 @@ var gulp = require('gulp');
 module.exports = function () {
     global.isWatching = true;
     gulp.watch('client/scss/**', ['sass']);
-    gulp.watch(['client/js/**/*.js', '!client/js/app.bundle.js'], ['lint', 'browserify']);
+    gulp.watch(['client/js/**/*.js', '!client/js/app.bundle.js', 'client/test/**/*.js', '!client/test/app.bundle.test.js'], ['lint', 'browserify', 'browserify_tests']);
 };
