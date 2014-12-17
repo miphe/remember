15b966470d895ba81fdcdd01a3e7ba86dff68657
André Drougge <andre.drougge@gmail.com>
2014-12-17 11:00:56 +0100

    Add entry leng restriction in list panel.

--- .dist/partials/entry-item.js
+++ .dist/partials/entry-item.js
@@ -9,7 +9,7 @@ module.run(['$templateCache', function($templateCache) {
     '<div class="panel panel-default" ng-class="{\'collapsed\' : entry.collapsed}">\n' +
     '    <div class="panel-body">\n' +
     '        <div class="media-body">\n' +
-    '            <p ng-bind-html="entry.content"></p>\n' +
+    '            <p ng-bind-html="entry.content | limitTo:180"></p>\n' +
     '        </div>\n' +
     '    </div>\n' +
     '    <entry-footer></entry-footer>\n' +
